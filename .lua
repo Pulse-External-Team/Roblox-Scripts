52a5722dc1f06e586c6c8c7127b02e6be5a3fad1
Pulse External Team <jqgsmfang@gmail.com>
2024-09-21 12:55:08 +0200

    .lua

---
+++ .lua
@@ -0,0 +1,8 @@
+wait(1)
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Pulse External is Injected";
+    Text = "Get Updates here https://github.com/Pulse-External-Team";
+    Icon = "";
+    Duration = "7";
+})
