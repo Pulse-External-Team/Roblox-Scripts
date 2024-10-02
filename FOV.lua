35fc498a217934f5bd8edfae50152371bbb0a7c9
Pulse External Team <jqgsmfang@gmail.com>
2024-10-02 15:29:29 +0200

    Add files via upload

---
+++ FOV.lua
@@ -0,0 +1,12 @@
+﻿local player = game.Players.LocalPlayer
+local camera = workspace.CurrentCamera
+
+-- Funktion, um das FOV zu setzen
+local function setFOV(newFOV)
+    camera.FieldOfView = newFOV
+end
+
+-- Beispiel: FOV auf 90 setzen
+setFOV(120)
+
+-- Du kannst den Wert hier anpassen
